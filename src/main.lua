dce29b878cdd49fb575bf7b7a86920c00bef2776
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-04-05 02:11:13 -0400

    READ DESC
    Added the following functions.
    `console.log(text)`
    `console.warn(text)`
    `console.error(text)`
    `console.info(text)`
    `console:Color(color)`

---
+++ src/main.lua
@@ -0,0 +1,32 @@
+local console = {}
+--> Setup Console
+rconsoleclear()
+rconsolename("Console")
+
+function console.log(text)
+    rconsoleprint(text .. "\n")
+end
+
+function console.warn(text)
+    rconsoleprint("@@YELLOW@@")
+    rconsoleprint(text .. "\n")
+    rconsoleprint("@@WHITE@@")
+end
+
+function console.error(text)
+    rconsoleprint("@@RED@@")
+    rconsoleprint(text .. "\n")
+    rconsoleprint("@@WHITE@@")
+end
+
+function console.info(text)
+    rconsoleprint("@@BLUE@@")
+    rconsoleprint(text .. "\n")
+    rconsoleprint("@@WHITE@@")
+end
+
+function console:Color(color)
+    rconsoleprint("@@" .. color .. "@@")
+end
+
+return console
